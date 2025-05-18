8be41a3d9f42f2cac8641b9c0c2870b63c622756
ZenX <tmgamig69@gmail.com>
2025-05-18 16:41:41 +0530

    init

--- src/main/java/com/dev/user_services_api_1/UserServicesApi1Application.java
+++ src/main/java/com/dev/user_services_api_1/UserServicesApi1Application.java
@@ -1,9 +1,12 @@
 package com.dev.user_services_api_1;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserServicesApi1Application {
 
 	public static void main(String[] args) {
